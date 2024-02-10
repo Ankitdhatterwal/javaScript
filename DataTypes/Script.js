2bc98410163f56324345a48847eac979fd01e7c9
Ankit Dhatterwal <90125082+Ankitdhatterwal@users.noreply.github.com>
2024-02-10 16:54:47 +0530

    Node.js practice

---
+++ DataTypes/Script.js
@@ -0,0 +1,18 @@
+var hello =10; 
+console.log(hello);
+hello = 'world';
+console.log(hello);
+
+let persion ={
+    name:"ankit",
+    age:23,
+    size:300,
+}
+console.log(persion.name);
+
+let isboolean = true
+console.log(isboolean);
+console.log(typeof isboolean);
+console.log(5 + '2');
+var practice = (5+"2") *4;
+console.log(practice);
---
+++ Wather_app/Script.js
@@ -0,0 +1,8 @@
+const button= document.getElementById("Search-button")
+const input= document.getElementById("input-city")
+button.addEventListener('click', () =>{
+
+
+console.log(input.value);
+
+})
